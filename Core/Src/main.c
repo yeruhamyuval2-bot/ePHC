c68286a4f28b90c1f2506b2ab34a59a719508099
yuval yeruham <yeruhamyuval2@gmail.com>
2026-01-21 11:55:42 +0200

    Fix case-sensitive include paths for Linux CI

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "Communication.h"
+#include "communication.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
